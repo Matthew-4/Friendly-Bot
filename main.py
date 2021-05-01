c8a055014df60ba7e43d5f868b0c071208c95d55
Matthew Pazhaidam <>
2021-05-01 16:31:33 +0000

    tweaks

--- main.py
+++ main.py
@@ -21,36 +21,47 @@ async def on_ready():
 async def hello(ctx):
   await ctx.send("Hey there! How are you doing?")
   check = True
-  global check
-
 
 #add more aliases
 @client.command(aliases = ['Good', 'great', 'Great'])
 async def good(ctx):
   await ctx.send("That's great! I wish you the best!")
   check = True
-  global check
 
+@client.command(aliases = ['Bad'])
+async def bad(ctx):
+  await ctx.send("Sorry to hear that :(! Hope it gets better!")
+  check = True
 
+@client.command(aliases = ['Okay'])
+async def okay(ctx):
+  await ctx.send("That's nice! Stick around and your day is going to be better than ever!")
+  check = True
 
+@client.command(aliases = ['Anime'])
 async def anime(ctx):
-  await ctx.send("You like anime? Me too! Name a genre you want, and i'll reccommend you some animes in that genre! All you have to do is type, /action, /romance, /horror, /other")
+  await ctx.send("You like anime? Me too! Name a genre you want, and i'll recommend you some animes in that genre! All you have to do is type, /action, /romance, /horror, /other")
+  check = True
 
-@client.command(aliases = ['action'])
+@client.command(aliases = ['Action'])
 async def action(ctx):
   await ctx.send("Some action animes are: One Piece, Naruto, Jujutsu Kaisen, Black Clover, Dororo, Demon Slayer, Hunter x Hunter, Attack on Titan, Tokyo Ghoul, Assasination Classroom")
+  check = True
 
-@client.command(aliases = ['romance'])
+@client.command(aliases = ['Romance'])
 async def romance(ctx):
   await ctx.send("Some romance animes are: Rascal Does Not Dream of Bunny Girl Senpai, Silent Voice, Your Lie in April, Oregairu, Your Name, Fly Me To The Moon, FireWorks, Plastic Memories")
+  check = True
 
-@client.command(aliases = ['horror'])
+@client.command(aliases = ['Horror'])
 async def horror(ctx):
   await ctx.send("Some horror animes are: Beyond the Boundary, Blue Exorcist, Full Metal Alchemist, Another, The Promised Neverland")
+  check = True
 
-@client.command(aliases = ['other'])
+@client.command(aliases = ['Other'])
 async def other(ctx):
   await ctx.send("Death Note, Code Geass: LeLouch of Rebellion, Dr.Stone, Tower of God, Classroom of Elite, Darling in the Franxx, Fairy Tail, Wise Mans Grandchild, Haikyuu!, Kuroko's Basketball")
+  check = True
 
 #add to the list of commands
 @client.command()
