f5bcf55629126fcd41efd3e486e6ccefe452da77
Matthew Pazhaidam <>
2021-05-01 15:29:40 +0000

    help command, unknown commands

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from replit import db
 
 #sets the prefix to all commands to "/"
 client = commands.Bot(command_prefix = "/")
+message_client = discord.Client()
+command_list = ['/hello', '/help', '/good', '/bad', 'okay', 'anime']
+check = False
 
 #tells us when the bot is ready
 @client.event
@@ -17,11 +20,14 @@ async def on_ready():
 @client.command(aliases = ['hi', 'Hi', 'Hello', 'sup', 'hey', 'Hey', 'Sup', 'wassup', 'Wassup'])
 async def hello(ctx):
   await ctx.send("Hey there! How are you doing?")
+  check = True
+
 
 #add more aliases
 @client.command(aliases = ['Good', 'great', 'Great'])
 async def good(ctx):
   await ctx.send("That's great! I wish you the best!")
+  check = True
 
 #add 'bad' command
 
@@ -31,8 +37,22 @@ async def good(ctx):
 @client.command()
 async def help(ctx):
   await ctx.send("Here are a list of some things you can ask me or command me to do: ")
-  await ctx.send("/hello (or other greetings)")
-  await ctx.send("/good, /bad - generic responses to 'How are you doing?'")
+  check = True
+  for command in command_list:
+    await ctx.send(command)
+  await ctx.sent("You can also say things similar to the words above.")
 
+@message_client.event()
+async def on_message(message):
+  global check
+  if message.author == client.user:
+    return
+
+  if check == True:
+    check = False
+  else:
+    await message.channel.send("Sorry I don't understand. Please type '/help' for a list of things you can say to me!")
+  
+  
 
 #add connection to the discord bot
